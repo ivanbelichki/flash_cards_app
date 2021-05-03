a4b9100c2da44a3d76e2c93f51703ecb43e5baee
ivanbelichki <ibelichki@gmail.com>
2021-05-02 22:43:31 -0400

    add comments

--- flash_cards.py
+++ flash_cards.py
@@ -1,5 +1,6 @@
 import random
 
+# CONSTANTS
 CARDS_FILE = 'cards.txt'
 QA_DIVIDER = ':'
 
@@ -11,7 +12,7 @@ QUIT_KEY = 'q'
 
 VALID_KEYS = [ADD_KEY, REMOVE_KEY, PLAY_KEY, VIEW_KEY, QUIT_KEY]
 
-
+# driver
 def __main__():
     choice = ''
     while choice != QUIT_KEY:
@@ -27,7 +28,9 @@ def __main__():
 
     print(f'\nSee you next time!\n')
 
+# start menu
 def start():
+    # print app menu
     print(f'''
             Welcome to the Flash Card App!
             Enter '{ADD_KEY}' to Add new Cards
@@ -36,84 +39,105 @@ def start():
             Enter '{PLAY_KEY}' to Play!
             or '{QUIT_KEY}' to Quit
             ''')
+
+    # get valid user choice
     user_in = ''
     while user_in not in VALID_KEYS:
         user_in = input('Enter your choice: ')
+
     return user_in
 
-
+# Flash Card Game Loop
 def play():
-    user_in = ''
+
     f = open(CARDS_FILE, "r")
     cards = f.readlines()
+
+    # keep track of used cards
     n = -1
     used = [-1]
+
+    # game loop
+    user_in = ''
     while user_in != QUIT_KEY:
 
-        if len(used) == len(cards) + 1:
+        # check if all cards used
+        all_cards_used = len(used) == len(cards) + 1
+        if all_cards_used:
             print('\n')
             print("Congrats! You've done all questions")
             return
 
+        # find random card index
         while n in used:
             n = random.randint(0, len(cards)-1)
-
         card = cards[n].split(QA_DIVIDER)
 
+        # print flash card
         print('\n')
         print(f'QUESTION: {card[0]}\n')
         input("Press 'Enter' for answer\n")
         print(f'ANSWER: {card[1]}')
 
+        # keep track of used card index
         used.append(n)
 
-
+        # get user choice
         user_in = input(f"Press 'Enter' for next question or {QUIT_KEY}' to quit: ")
 
     f.close()
 
-
+# add flash cards to deck
 def add_cards():
 
     count = 0
+
+    # adding card loop
     user_in = ''
     while user_in != QUIT_KEY:
 
+        # enter question
         user_in = input(f'Enter the question, or {QUIT_KEY} to quit: ')
         if user_in == QUIT_KEY:
             break
 
         question = user_in
 
+        # enter answer
         user_in = input(f'Enter the answer, or {QUIT_KEY} to quit: ')
         if user_in == QUIT_KEY:
             break
 
         answer = user_in
 
+        # write to flash card to file
         write_to_file(question, answer)
+
+        # print flash card
         print(f'''
             Flash Card entered:
             Q: {question}
             A: {answer}
         ''')
+
         count += 1
 
     if count > 0:
         print(f'Thanks for entering {count} card(s)')
 
-
+# remove flash cards from deck
 def remove_cards():
 
     choice = ''
     while choice != QUIT_KEY:
+
+        # print flash card deck
         view_cards()
 
-        f = open(CARDS_FILE, "r")
-        cards = f.readlines()
-
+        # get user input to delete card
         print('\n')
         choice = input(f'''Enter the number of the Card you wish to remove, or {QUIT_KEY} to quit ''')
+
         if choice == QUIT_KEY:
             break
         try:
@@ -122,12 +146,17 @@ def remove_cards():
             print('Please Enter a valid Card number')
             continue
 
-        if (choice) > len(cards) or choice < 0:
+        f = open(CARDS_FILE, "r")
+        cards = f.readlines()
+
+        if choice > len(cards) or choice < 0:
             print('Please Enter a valid Card number')
             continue
 
+        # delete card from data strucutre
         cards.remove(cards[choice])
 
+        # overwrite card file with new cards
         w = open(CARDS_FILE, "w")
         w.write(''.join(cards))
         w.close()
@@ -135,20 +164,25 @@ def remove_cards():
         f.close()
 
 
-
+# print flash cards
 def view_cards():
         f = open(CARDS_FILE, "r")
-        print('\n')
         cards = f.readlines()
+
+        print('\n')
         print(f'{len(cards)} cards:')
         print('\n')
 
+        # get [question, answer] list of flash cards
         cards = [card.split(QA_DIVIDER) for card in cards]
+
+        # list cards by number
         for i, card in enumerate(cards):
             print(f'{i}) QUESTION: {card[0]}, ANSWER: {card[1]}')
 
         f.close()
 
+# write flash card to file
 def write_to_file(question, answer):
     f = open(CARDS_FILE, "a")
     f.write(question + QA_DIVIDER + answer + '\n')
